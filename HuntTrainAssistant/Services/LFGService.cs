b950afd2d83010f1ea2ce58dce554a2b8a1c58ac
Limiana <5073202+Limiana@users.noreply.github.com>
2025-08-07 05:07:10 +0300

    Remove useless stuff

--- HuntTrainAssistant/Services/LFGService.cs
+++ HuntTrainAssistant/Services/LFGService.cs
@@ -11,9 +11,6 @@ using AtkEvent = FFXIVClientStructs.FFXIV.Component.GUI.AtkEvent;
 namespace HuntTrainAssistant.Services;
 public unsafe class LFGService : IDisposable
 {
-    delegate nint AgentLookingForGroup_ReceiveEvent(nint a1, nint a2, nint a3, uint a4, nint a5);
-    [EzHook("48 89 5C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 41 56 48 83 EC 40 45 8B D1")]
-    EzHook<AgentLookingForGroup_ReceiveEvent> AgentLookingForGroup_ReceiveEventHook;
     private LFGService()
     {
         Svc.AddonLifecycle.RegisterListener(AddonEvent.PostReceiveEvent, "LookingForGroupCondition", OnReceiveEvent);
@@ -23,21 +20,6 @@ public unsafe class LFGService : IDisposable
     public void Dispose()
     {
         Svc.AddonLifecycle.RegisterListener(AddonEvent.PostReceiveEvent, "LookingForGroupCondition", OnReceiveEvent);
-    }
-
-    nint AgentLookingForGroup_ReceiveEventDetour(nint a1, nint a2, nint a3, uint a4, nint a5)
-    {
-        try
-        {
-            if(a5 == 3)
-            {
-                PluginLog.Information($"AgentLookingForGroup_ReceiveEventDetour: {a2:X}б {a3:X}, {a4}, {a5:X}");
-                var str = (AtkValue*)(a2 + 16);
-                PluginLog.Information($"   {MemoryHelper.ReadStringNullTerminated((nint)(a1+ 9680))}/{MemoryHelper.ReadRaw(a3, 40).ToArray().ToHexString()}");
-            }
-        }
-        catch(Exception e) { e.Log(); }
-        return AgentLookingForGroup_ReceiveEventHook.Original(a1, a2, a3, a4, a5);
     }
 
     private void OnReceiveEvent(AddonEvent type, AddonArgs args)
