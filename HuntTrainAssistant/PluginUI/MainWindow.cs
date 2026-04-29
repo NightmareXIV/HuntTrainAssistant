a2e631ce654d12d8313b1e4cefef61d678fdecec
Limiana <5073202+Limiana@users.noreply.github.com>
2026-04-30 02:53:50 +0300

    Commit all changes

--- HuntTrainAssistant/PluginUI/MainWindow.cs
+++ HuntTrainAssistant/PluginUI/MainWindow.cs
@@ -1,4 +1,5 @@
 ﻿using Dalamud.Interface.Style;
+using ECommons.ExcelServices;
 using ECommons.ImGuiMethods;
 using ECommons.SimpleGui;
 using HuntTrainAssistant.Tasks;
@@ -28,93 +29,100 @@ public unsafe class MainWindow : ConfigWindow
 
     public override void Draw()
     {
-				ImGui.SetNextItemWidth(150f);
-				var condIndex = 0;
-				var condNames = P.Config.Conductors.Select(x => x.Name).ToArray();
-				ImGuiEx.Text("Current conductors:");
-				ImGui.SameLine();
-				if (ImGui.SmallButton("Clear"))
+				try
 				{
-						P.Config.Conductors.Clear();
-				}
-				ImGui.SameLine();
-				// Remove selected conductor
-				if (ImGui.SmallButton("Remove selected"))
-				{
-						if (condIndex >= 0 && condIndex < P.Config.Conductors.Count)
-						{
-								P.Config.Conductors.RemoveAt(condIndex);
-						}
-				}
-				ImGuiEx.SetNextItemFullWidth();
-				ImGui.ListBox("##conds", ref condIndex, condNames, Math.Clamp(condNames.Length, 1, 3));
-				ImGuiEx.Text("Add conductor:");
-				ImGui.SameLine();
-				ImGui.SetNextItemWidth(150f);
-				var newCond = "";
-				if (ImGui.InputText("##newCond", ref newCond, 50, ImGuiInputTextFlags.EnterReturnsTrue))
-				{
-						if (newCond.Length > 0)
-						{
-								P.Config.Conductors.Add(new(newCond, 0));
-								newCond = "";
-						}
-				}
-				if (P.TeleportTo == null)
-				{
-						ImGuiEx.Text(ImGuiColors.DalamudGrey3, "Autoteleport: inactive");
-						if (ChatMessageHandler.LastMessageLoc != null && ImGui.Button($"Autoteleport to {ChatMessageHandler.LastMessageLoc.Aetheryte.PlaceName.Value.Name}"))
-						{
-								P.TeleportTo = ChatMessageHandler.LastMessageLoc;
-						}
-				}
-				else
-				{
-						ImGuiEx.Text($"Autoteleport active.");
+						ImGui.SetNextItemWidth(150f);
+						var condIndex = 0;
+						var condNames = P.Config.Conductors.Select(x => x.Name).ToArray();
+						ImGuiEx.Text("Current conductors:");
 						ImGui.SameLine();
-						if (ImGui.SmallButton("Cancel"))
-            {
-                PluginLog.Debug($"TeleportTo reset (3)");
-                P.TeleportTo = null;
-						}
-						ImGuiEx.Text($"{P.TeleportTo.Aetheryte.GetPlaceName()}@{P.TeleportTo.Territory.GetTerritoryName()} i{P.TeleportTo.Instance}");
-				}
-				if(P.TaskManager.IsBusy)
-				{
-						ImGuiEx.Text($"{P.TaskManager.NumQueuedTasks:D2} tasks processing");
-            ImGui.SameLine();
-            if(ImGui.SmallButton("Stop##tm"))
-            {
-								P.TaskManager.Abort();
-            }
-        }
-				else
-				{
-						ImGuiEx.Text(ImGuiColors.DalamudGrey3, $"Task manager: inactive");
-				}
-						ImGui.Checkbox($"Sonar Auto-teleport", ref P.Config.AutoVisitTeleportEnabled);
-				if (P.Config.AutoVisitTeleportEnabled)
-				{
-						if (!Utils.IsInHuntingTerritory())
+						if(ImGui.SmallButton("Clear"))
 						{
-								ImGuiEx.HelpMarker("You are not in a hunting zone. Teleport enabled.", EColor.GreenBright, FontAwesomeIcon.Check.ToIconString());
+								P.Config.Conductors.Clear();
+						}
+						ImGui.SameLine();
+						// Remove selected conductor
+						if(ImGui.SmallButton("Remove selected"))
+						{
+								if(condIndex >= 0 && condIndex < P.Config.Conductors.Count)
+								{
+										P.Config.Conductors.RemoveAt(condIndex);
+								}
+						}
+						ImGuiEx.SetNextItemFullWidth();
+						ImGui.ListBox("##conds", ref condIndex, condNames, Math.Clamp(condNames.Length, 1, 3));
+						ImGuiEx.Text("Add conductor:");
+						ImGui.SameLine();
+						ImGui.SetNextItemWidth(150f);
+						var newCond = "";
+						if(ImGui.InputText("##newCond", ref newCond, 50, ImGuiInputTextFlags.EnterReturnsTrue))
+						{
+								if(newCond.Length > 0)
+								{
+										P.Config.Conductors.Add(new(newCond, 0));
+										newCond = "";
+								}
+						}
+						if(P.TeleportTo == null)
+						{
+								ImGuiEx.Text(ImGuiColors.DalamudGrey3, "Autoteleport: inactive");
+								if(ChatMessageHandler.LastMessageLoc != null && ImGui.Button($"Autoteleport to {ChatMessageHandler.LastMessageLoc.Aetheryte.PlaceName.Value.Name}"))
+								{
+										P.TeleportTo = ChatMessageHandler.LastMessageLoc;
+								}
 						}
 						else
 						{
-								ImGuiEx.HelpMarker("You are in a hunting zone. Teleport disabled. ", EColor.RedBright, "\uf00d");
+								ImGuiEx.Text($"Autoteleport active.");
+								ImGui.SameLine();
+								if(ImGui.SmallButton("Cancel"))
+								{
+										PluginLog.Debug($"TeleportTo reset (3)");
+										P.TeleportTo = null;
+								}
+								ImGuiEx.Text($"{P.TeleportTo?.Aetheryte.GetPlaceName()}@{ExcelTerritoryHelper.GetName(P.TeleportTo?.Territory ?? 0)} i{P.TeleportTo?.Instance}");
 						}
-						ImGui.SameLine();
-						ImGui.Checkbox("C/W", ref P.Config.AutoVisitCrossWorld);
-						ImGui.SameLine();
-						ImGui.Checkbox("C/DC", ref P.Config.AutoVisitCrossDC);
-				}
-				if(S.SonarMonitor.Continuation != null)
-				{
-						ImGuiEx.Text(GradientColor.Get(EColor.RedBright, EColor.YellowBright), $"Waiting to arrive at: {S.SonarMonitor.Continuation.World}/{S.SonarMonitor.Continuation.Aetheryte.GetPlaceName()} i{S.SonarMonitor.Continuation.Instance}");
-						if (ImGui.SmallButton("Cancel##arrival"))
+						if(P.TaskManager.IsBusy)
 						{
-								S.SonarMonitor.Continuation = null;
+								ImGuiEx.Text($"{P.TaskManager.NumQueuedTasks:D2} tasks processing");
+								ImGui.SameLine();
+								if(ImGui.SmallButton("Stop##tm"))
+								{
+										P.TaskManager.Abort();
+								}
 						}
+						else
+						{
+								ImGuiEx.Text(ImGuiColors.DalamudGrey3, $"Task manager: inactive");
+						}
+						ImGui.Checkbox($"Sonar Auto-teleport", ref P.Config.AutoVisitTeleportEnabled);
+						if(P.Config.AutoVisitTeleportEnabled)
+						{
+								if(!Utils.IsInHuntingTerritory())
+								{
+										ImGuiEx.HelpMarker("You are not in a hunting zone. Teleport enabled.", EColor.GreenBright, FontAwesomeIcon.Check.ToIconString());
+								}
+								else
+								{
+										ImGuiEx.HelpMarker("You are in a hunting zone. Teleport disabled. ", EColor.RedBright, "\uf00d");
+								}
+								ImGui.SameLine();
+								ImGui.Checkbox("C/W", ref P.Config.AutoVisitCrossWorld);
+								ImGui.SameLine();
+								ImGui.Checkbox("C/DC", ref P.Config.AutoVisitCrossDC);
+						}
+						if(S.SonarMonitor.Continuation != null)
+						{
+								ImGuiEx.Text(GradientColor.Get(EColor.RedBright, EColor.YellowBright), $"Waiting to arrive at: {S.SonarMonitor.Continuation.World}/{S.SonarMonitor.Continuation.Aetheryte.GetPlaceName()} i{S.SonarMonitor.Continuation.Instance}");
+								if(ImGui.SmallButton("Cancel##arrival"))
+								{
+										S.SonarMonitor.Continuation = null;
+								}
+						}
+				}
+				catch(Exception e)
+				{
+						e.LogWarning();
 				}
     }
 
